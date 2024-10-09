65a6a0b35475130fbf6ccc087ac8d578259093dc
Messorah <garabrielra@gmail.com>
2024-10-09 17:35:18 -0300

    templates/css

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog import views
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', views.index, name="index"),
+    path('post/', views.post, name="post"),
+    path('page/', views.page, name="page"),
 ]
